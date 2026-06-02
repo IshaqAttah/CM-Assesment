83b74a0dfa7d5b72dba0de8a2cb3c7c7a5387bd1
Ishaqmusa <musaishaq99@gmail.com>
2026-06-02 18:25:57 +0100

    added terraform files for ingress

---
+++ terraform/modules/ingress/main.tf
@@ -0,0 +1,13 @@
+resource "helm_release" "nginx_ingress" {
+  name             = "ingress-nginx"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  version          = "4.9.0"
+  namespace        = "ingress-nginx"
+  create_namespace = true
+
+  set { name = "controller.service.type";             value = "NodePort" }
+  set { name = "controller.service.nodePorts.http";   value = "80" }
+  set { name = "controller.service.nodePorts.https";  value = "443" }
+  set { name = "controller.podAnnotations.linkerd\\.io/inject"; value = "enabled" }
+}
