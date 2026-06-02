032526d716f935c350ac88ed36daa241d463dd86
Ishaqmusa <musaishaq99@gmail.com>
2026-06-02 21:26:38 +0100

    removed invalid nodePort values from ingress module

--- terraform/modules/ingress/main.tf
+++ terraform/modules/ingress/main.tf
@@ -1,26 +1,18 @@
 resource "helm_release" "nginx_ingress" {
-  name             = "ingress-nginx"
-  repository       = "https://kubernetes.github.io/ingress-nginx"
-  chart            = "ingress-nginx"
-  version          = "4.9.0"
-  namespace        = "ingress-nginx"
-  create_namespace = true
+    name             = "ingress-nginx"
+    repository       = "https://kubernetes.github.io/ingress-nginx"
+    chart            = "ingress-nginx"
+    version          = "4.9.0"
+    namespace        = "ingress-nginx"
+    create_namespace = true
 
-  set {
-    name  = "controller.service.type"
-    value = "NodePort"
+    set {
+      name  = "controller.service.type"
+      value = "NodePort"
+    }
+    
+    set {
+      name  = "controller.podAnnotations.linkerd\\.io/inject"
+      value = "enabled"
+    }
   }
-  set {
-    name  = "controller.service.nodePorts.http"
-    value = "80"
-  } 
-  set {
-    name  = "controller.service.nodePorts.https"
-    value = "443"
-  } 
-  
-  set {
-    name  = "controller.podAnnotations.linkerd\\.io/inject"
-    value = "enabled"
-  }
-}
