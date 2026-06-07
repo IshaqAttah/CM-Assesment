13b648a473dfc92702d292367aafeadda98e5061
Ishaqmusa <musaishaq99@gmail.com>
2026-06-07 13:27:36 +0100

    fix: scan image before pushing to registry

--- shell/script.sh
+++ shell/script.sh
@@ -1,9 +1,20 @@
 STDOUT="/dev/stdout"
-LOG_FILE="$STDOUT"                                        
+LOG_FILE=$STDOUT                                        
 LOG_MESSAGE="$(date) is the date, should log to $STDOUT" 
 
 log_message() {
-    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" >> "$LOG_FILE"  
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" >> $LOG_FILE  
 }
 
-log_message "$LOG_MESSAGE"  
+log_message $LOG_MESSAGE 
+
+# STDOUT="/dev/stdout"
+# LOG_FILE='$STDOUT'
+# LOG_MESSAGE='is the date, should log to $STDOUT'
+
+# # log with timestamp
+# log_message() {
+#     echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" >> "$LOGFILE"
+# }
+
+# log_message $LOG_MESSAGE 
